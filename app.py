3f9a73b607d7c6c538a9b8236371b1b3f6077e69
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-01-28 18:41:16 +0330

    feat: improve the logic

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import sys
-import os
 import ctypes
 from urllib.parse import urlparse
 from tkinter import filedialog
@@ -7,9 +6,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 
-# Get the screen width
-user32 = ctypes.windll.user32
-screen_width = user32.GetSystemMetrics(0)
+# Get the url from command line arguments
+url = sys.argv[1]
+
+# Get the screen's width
+screen_width = ctypes.windll.user32.GetSystemMetrics(0)
 
 # Define the driver options
 options = webdriver.ChromeOptions()
@@ -18,15 +19,8 @@ options.add_argument('--headless')
 # Initialize the driver
 browser = webdriver.Chrome(options=options)
 
-
-# Select a directory to save the file
-print("Selecting output directory...")
-selected_path = filedialog.askdirectory(title='Select output directories')
-
 browser.maximize_window()
 
-# Get the url from command line arguments
-url = sys.argv[1]
 
 # Get the web page
 browser.get(url)
@@ -35,18 +29,29 @@ browser.get(url)
 page_height = browser.execute_script(
     'return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);')
 
-# Set width and height od of thw window based on screen's width and web page's height
+# Set the width and height of the window based on the screen width and the web page's height
 browser.set_window_size(screen_width, page_height)
 
 page_body = browser.find_element(By.TAG_NAME, 'body')
 
-try:
-    # Extract the domain name from the given url
-    domain_name = urlparse(url).netloc
+# Extract the domain name from the given url
+domain_name = urlparse(url).netloc
 
-    page_body.screenshot(os.path.join(selected_path, f'{domain_name}.png'))
+# Select a directory to save the file
+file_name = filedialog.asksaveasfilename(filetypes=[('PNG', '*.png')])
+
+# Check if file name is not selected
+if not file_name:
+    print('Operation aborted!')
+    exit()
+
+file_name += '.png'
+
+try:
+    page_body.screenshot(file_name)
 except WebDriverException as ex:
-    print(ex)
+    print(f"An unexpected error occurred: {ex}")
+else:
+    print('The screenshot was saved successfully at ' + file_name)
 finally:
     browser.quit()
-    print('The screenshot was saved successfully at ' + selected_path)
